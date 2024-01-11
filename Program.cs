4e9ca5d81e983495694703f17961a931fb4f45f8
Bandile Shaun Baqwa <bsbaqwa@gmail.com>
2024-01-11 11:37:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace FizzBuzzConsoleApp
+{
+    delegate void FizzBuzzOutput(string output);
+
+    class Program
+    {
+        static void DisplayFizzBuzz(string output)
+        {
+            Console.WriteLine(output);
+        }
+
+        static void Main(string[] agrs)
+        {
+            Run(DisplayFizzBuzz, 1, 100);
+            Console.ReadKey();
+        }
+
+        public static void Run(FizzBuzzOutput output, int start, int increment)
+        {
+            for (var i = start; i < start + increment; i++)
+            {
+                var mod4 = i % 4 == 0;
+                var mod6 = i % 6 == 0;
+
+                if (mod4 && mod6)
+                    output("FizzBuzz");
+                else if (mod4)
+                    output("Fizz");
+                else if (mod6)
+                    output("Buzz");
+                else
+                    output(i.ToString());
+
+
+            }
+        }
+    }
+}
